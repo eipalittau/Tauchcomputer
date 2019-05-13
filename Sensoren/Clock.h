998903931f6b6a7f9fc6a2798111f57b6a439296
eipalittau <eipa@gmx.ch>
2019-05-13 12:21:35 +0200

    Update Clock.h

--- Sensoren/Clock.h
+++ Sensoren/Clock.h
@@ -1,1 +1,23 @@
+#ifndef __clock_h_
+  #define __clock_h_
+  
+  #include <I2CBase.h>
+#endif
 
+class Clock {
+public:
+  struct DataStruct {
+    int Second;
+    int Minute;
+    int Hour;
+    int Day;
+    int Month;
+    int Year;
+    String Weekday;
+  };
+  
+  Clock();
+  ~Clock();
+
+  void Clock::GetData(struct DataStruct *aData);
+};
