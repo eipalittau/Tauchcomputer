7ec327484378f31d4045b89a1716bd2ef503ea92
eipalittau <eipa@gmx.ch>
2019-05-13 12:22:26 +0200

    Update Clock.cpp

--- Sensoren/Clock.cpp
+++ Sensoren/Clock.cpp
@@ -1,1 +1,19 @@
+class Clock {
+public:
+  struct DataStruct {
+    int Second;
+    int Minute;
+    int Hour;
+    int Day;
+    int Month;
+    int Year;
+    String Weekday;
+  };
+  
+  Clock() {}
+  
+  ~Clock() {}
 
+  void Clock::GetData(struct DataStruct *aData) {
+    
+  }
