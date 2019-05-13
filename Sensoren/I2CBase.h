0b1ec96279df6ee759f2ab939c31f036d5583b52
eipalittau <eipa@gmx.ch>
2019-05-13 11:37:49 +0200

    Update I2CBase.h

--- Sensoren/I2CBase.h
+++ Sensoren/I2CBase.h
@@ -1,1 +1,11 @@
+#ifndef __i2cbase_h_
+  #define __i2cbase_h_
+  
+  #include <Arduino.h>
+  #include <Wire.h>
+  #include "config.h"
 
+  #define TIMEOUT 100
+
+  void GetData(uint8_t aAdress, uint8_t aRegister, int aBytes);
+#endif
