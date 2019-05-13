84ba8a487c552e71dd3be039eda533166bd4def2
eipalittau <eipa@gmx.ch>
2019-05-13 22:44:51 +0200

    Update Clock.cpp

--- Sensoren/Clock.cpp
+++ Sensoren/Clock.cpp
@@ -1,25 +1,32 @@
 #include "Clock.h"
 
 //Constructor / Destructor
-Clock::Clock() {}
+Clock::Clock() {
+  mBase = new I2CBase(0x00, 7);
+}
 
 Clock::~Clock() {}
 
 //Public
 void Clock::SetData(struct DataStruct aData) {
+  int lTimestamp[7] = {aData->Second, aData->Minute, aData->Hour, aData->Weekday, aData->Day, aData->Month, aData->Year};
   
+  lTimestamp[6] -= 2000;
+  
+  I2CBase.SetData(lTimestamp);
 }
 
 void Clock::GetData(struct DataStruct *aData) {
   int lTimestamp[7];
   
-  if (I2CBase.GetData(0x68, 0x00, lTimestamp) == 0) {  
+  if (I2CBase.GetData(lTimestamp) == 0) {  
     aData->Second = lTimestamp[0];
     aData->Minute = lTimestamp[1];
     aData->Hour = lTimestamp[2];
+    aData->Weekday = lTimestamp[3];
     aData->Day = lTimestamp[4];
     aData->Month = lTimestamp[5];
     aData->Year = lTimestamp[6] + 2000;
-    aData->Weekday = lTimestamp[3];
+
   }
 }
