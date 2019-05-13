6db4b6349bd520d0c535af772a1e527ce6f65302
eipalittau <eipa@gmx.ch>
2019-05-13 12:16:00 +0200

    Update I2CBase.cpp

--- Sensoren/I2CBase.cpp
+++ Sensoren/I2CBase.cpp
@@ -9,43 +9,43 @@ I2CBase::~I2CBase() {}
 // 0 = alles OK
 // 1 = Keine Daten
 int I2CBase::GetData(int aAdress, int aRegister, int *aData[]) {
-	int lSize = sizeof(aData) / sizeof(int);
-	int lI;
+  int lSize = sizeof(aData) / sizeof(int);
+  int lI;
 
-	if (HasData(aAdress, aRegister, lSize)) {
-		for (lI = 0; lI < lSize; lI++) {
-			aData[lI] = Bcd2Dec(Wire.read());
-		}
-    
+  if (HasData(aAdress, aRegister, lSize)) {
+    for (lI = 0; lI < lSize; lI++) {
+      aData[lI] = Bcd2Dec(Wire.read());
+    }
+
     return 0;
-	} else {
+  } else {
     return 1;
   }
 }
 
 //Private
 bool I2CBase::HasData(int aAdress, int aRegister, int aBytes) {
-	Wire.beginTransmission(aAdress);
-	Wire.write(aRegister);
-	Wire.endTransmission();
+  Wire.beginTransmission(aAdress);
+  Wire.write(aRegister);
+  Wire.endTransmission();
 
-	int lStart = millis();
+  int lStart = millis();
 
-	while (millis() - lStart < TIMEOUT) {
-		if (Wire.requestFrom(aAdress, aBytes) == aBytes) {
-			return true;
-		}
+  while (millis() - lStart < TIMEOUT) {
+    if (Wire.requestFrom(aAdress, aBytes) == aBytes) {
+      return true;
+    }
 
-		delay(2);
-	}
+    delay(2);
+  }
 
-	return false;
+  return false;
 }
 
 int I2CBase::Dec2Bcd(const int aValue) {
-	return (aValue * 1.6) + (aValue % 10);
+  return (aValue * 1.6) + (aValue % 10);
 }
 
 int I2CBase::Dec2Bcd(const int aValue) {
-	return (aValue / 160) + (aValue % 16);
+  return (aValue / 160) + (aValue % 16);
 }
