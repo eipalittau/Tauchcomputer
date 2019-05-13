b79f4521d06bac30b5148d79c558f31f35516796
eipalittau <eipa@gmx.ch>
2019-05-13 12:13:15 +0200

    Update I2CBase.cpp

--- Sensoren/I2CBase.cpp
+++ Sensoren/I2CBase.cpp
@@ -1,12 +1,51 @@
 #include "I2CBase.h"
 
-I2CBase::I2CBase() {
+//Constructor / Destructor
+I2CBase::I2CBase() {}
+
+I2CBase::~I2CBase() {}
+
+//Public
+// 0 = alles OK
+// 1 = Keine Daten
+int I2CBase::GetData(int aAdress, int aRegister, int *aData[]) {
+	int lSize = sizeof(aData) / sizeof(int);
+	int lI;
+
+	if (HasData(aAdress, aRegister, lSize)) {
+		for (lI = 0; lI < lSize; lI++) {
+			aData[lI] = Bcd2Dec(Wire.read());
+		}
+    
+    return 0;
+	} else {
+    return 1;
+  }
 }
 
+//Private
+bool I2CBase::HasData(int aAdress, int aRegister, int aBytes) {
+	Wire.beginTransmission(aAdress);
+	Wire.write(aRegister);
+	Wire.endTransmission();
 
-I2CBase::~I2CBase() {
+	int lStart = millis();
+
+	while (millis() - lStart < TIMEOUT) {
+		if (Wire.requestFrom(aAdress, aBytes) == aBytes) {
+			return true;
+		}
+
+		delay(2);
+	}
+
+	return false;
 }
 
-void I2CBase::GetData(int aAdress, int aRegister, int aBytes) {
+int I2CBase::Dec2Bcd(const int aValue) {
+	return (aValue * 1.6) + (aValue % 10);
+}
 
+int I2CBase::Dec2Bcd(const int aValue) {
+	return (aValue / 160) + (aValue % 16);
 }
