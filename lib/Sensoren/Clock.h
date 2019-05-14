bd9f09a98d226fc72435ea78c6278e3a2c244388
eipalittau <eipa@gmx.ch>
2019-05-14 08:57:43 +0200

    Add files via upload

---
+++ lib/Sensoren/Clock.h
@@ -0,0 +1,27 @@
+#ifndef __clock_h_
+  #define __clock_h_
+  
+  #include <I2CBase.h>
+#endif
+
+class Clock {
+public:
+  struct DataStruct {
+    int Second;
+    int Minute;
+    int Hour;
+    int Day;
+    int Month;
+    int Year;
+    String Weekday;
+  };
+  
+  Clock();
+  ~Clock();
+  
+  void Clock::SetData(struct DataStruct aData);
+  void Clock::GetData(struct DataStruct *aData);
+  
+private:
+  I2CBase mBase;
+};
---
+++ lib/Sensoren/I2CBase.h
@@ -0,0 +1,26 @@
+#ifndef __i2cbase_h_
+  #define __i2cbase_h_
+  
+  #include <Arduino.h>
+  #include <Wire.h>
+  #include "config.h"
+  
+  #define TIMEOUT 100
+#endif
+
+class I2CBase {
+public:
+  I2CBase(int aAdress, int aRegister);
+  ~I2CBase();
+
+  void I2CBase::SetData(int aData[]);
+  int  I2CBase::GetData(int *aData[]);
+  
+private:
+  int mAdress;
+  int mRegister;
+  
+  bool I2CBase::HasData(int aBytes);
+  int  I2CBase::Dec2Bcd(int aValue);
+  int  I2CBase::Dec2Bcd(int aValue);
+};
