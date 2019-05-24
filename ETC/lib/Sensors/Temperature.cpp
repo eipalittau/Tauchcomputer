96f2f809a51a02b0980a7d66dbeb3e4d0d01d1b6
Patrik Eichmann <eipa@gmx.ch>
2019-05-24 12:44:42 +0200

    Update Temperature.cpp

--- ETC/lib/Sensors/Temperature.cpp
+++ ETC/lib/Sensors/Temperature.cpp
@@ -1,46 +1,21 @@
 #include "Temperature.h"
 
-const unsigned char TIMEOUT = 100;
-
-OneWire m1Wire(2);
-
 //Constructor / Destructor
-Temperature::Temperature() {}
+Temperature::Temperature() : WireBase(2) {}
 
 Temperature::~Temperature() {}
 
 //Public
-float Temperature::GetData() {
-	ScratchPad lScratchPad;
-
-	if (SendCommand(0x44)) {
-		unsigned long lStart = millis();
-
-		while (m1Wire->read_bit() != 1 && millis() - lStart < Constants.TIMEOUT) {
-			delay(2)
-		}
-		
-		if (SendCommand(0xBE)) {
-			for (unsigned char lI = 0; lI < 9; lI++) {
-				lScratchPad[i] = m1Wire->read();
-			}
-
-			return (float) ((((signed short)lScratchPad[1]) << 11) | (((signed short)lScratchPad[0]) << 3)) * 0.0078125;
-		}
-	}
-
-	return numeric_limits<float>::min();
+void Temperature::StartMesurement() {
+	WireBase::StartMesurement(0x44);
 }
-	
-	
-//Private
-bool Temperature::SendCommand(unsigned char aValue) {
-	if (m1Wire->reset() == 0) {
-		return false;
+
+float Temperature::GetData() {
+	signed short lData[9]
+		
+	if (WireBase::GetData(0xBE, lData) = 0) {
+		return (float) ((lData[1] << 11) | (lData[0] << 3)) * 0.0078125;
+	} else {
+		return numeric_limits<float>::min();
 	}
-	
-	m1Wire->select(deviceAddress);
-	m1Wire->write(aValue);
-	
-	return true;
 }
