28decf2e505338b99b655910d383dfae7222d622
Patrik Eichmann <eipa@gmx.ch>
2019-05-24 12:05:29 +0200

    Update I2CBase.h

--- ETC/lib/Sensors/I2CBase.h
+++ ETC/lib/Sensors/I2CBase.h
@@ -6,16 +6,16 @@
 #include "Constants.h"
 
 class I2CBase {
-public:
+protected:
 	I2CBase(unsigned char mI2CAdress, unsigned char aRegister);
 	~I2CBase();
-
+	
+	void StartMeasurement();
 	void SetData(unsigned char aData[]);
-	signed char GetData(unsigned char aData[]);
-	unsigned char Dec2Hex(unsigned char aValue);
-	unsigned char Hex2Dec(unsigned char aValue);
+	unsigned char GetData(unsigned char aData[]);
 
 private:
-	bool HasData(unsigned short int aSize);
+	unsigned char Dec2Hex(unsigned char aValue);
+	unsigned char Hex2Dec(unsigned char aValue);
 };
 #endif
