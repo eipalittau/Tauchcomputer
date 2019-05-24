74f080d6dff78f43533913e1227763ef892ba897
Patrik Eichmann <eipa@gmx.ch>
2019-05-24 12:14:38 +0200

    Update ISensor.h

--- ETC/lib/Sensors/ISensor.h
+++ ETC/lib/Sensors/ISensor.h
@@ -1,21 +1,15 @@
-class Figure
-{
+#ifndef _ISENSOR_h
+#define _ISENSOR_h
+
+class ISensor {
 protected:
-   Figure() {}
- 
+	ISensor();
+   
 public:
-   virtual ~Figure() {}
- 
-   // Aufbau:
-   // virtual Rückgabetyp nameDerFunktion(Variable1, Variable2, VariableN) = 0;
-   // mit dem "= 0" zwingt man den Programmierer die Methode zu implementieren!!!
-   // Sonst gibt es Fehler beim Kompilieren...
-   virtual double getX() = 0;
-   virtual double getY() = 0;
- 
-   virtual void setX(double x) = 0;
-   virtual void setY(double y) = 0;
- 
-   virtual short scaleUp(int factor) = 0;
-   virtual short scaleDown(int factor) = 0;
+	virtual ~ISensor();
+	
+	virtual void StartMeasurement() = 0;
+	virtual void SetData(unsigned char aData[]) = 0;
+	virtual unsigned char GetData(unsigned char aData[]) = 0;
 };
+#endif
