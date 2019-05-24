c8510599575b52028ef54525852c833ac17cbc11
Patrik Eichmann <eipa@gmx.ch>
2019-05-24 12:44:17 +0200

    Update WireBase.cpp

--- ETC/lib/Sensors/WireBase.cpp
+++ ETC/lib/Sensors/WireBase.cpp
@@ -4,7 +4,7 @@ OneWire m1Wire;
 
 //Constructor / Destructor
 WireBase::WireBase(unsigned char aPinNumber) {
-  m1Wire(aPinNumber);
+	m1Wire(aPinNumber);
 }
 
 WireBase::~WireBase() {}
@@ -15,26 +15,25 @@ void WireBase::StartMesurement(unsigned char aCommand) {
 }
 
 unsigned char WireBase::GetData(unsigned char aCommand, unsigned short aData[]) {
-  unsigned long lStart = millis();
+	unsigned long lStart = millis();
 
-  while (m1Wire->read_bit() != 1 && millis() - lStart < Constants.TIMEOUT) {
-    delay(2)
-  }
-
-  if (SendCommand(aCommand)) {
-    unsigned short lSize = sizeof(aData) / sizeof(unsigned char);
-    
-    for (unsigned char lI = 0; lI < lSize; lI++) {
-      aData[i] = m1Wire->read();
-    }
-    
-    return 0;
-  }
-
+	while (m1Wire->read_bit() != 1 && millis() - lStart < Constants.TIMEOUT) {
+		delay(2)
+	}
+	
+	if (SendCommand(aCommand)) {
+		unsigned short lSize = sizeof(aData) / sizeof(unsigned char);
+		
+		for (unsigned char lI = 0; lI < lSize; lI++) {
+			aData[i] = m1Wire->read();
+		}
+		
+		return 0;
+	}
+	
 	return 1;
 }
-	
-	
+
 //Private
 bool WireBase::SendCommand(unsigned char aValue) {
 	if (m1Wire->reset() == 0) {
