eed377cf45df961b7aa89d856c0c170f35bc8026
Patrik Eichmann <eipa@gmx.ch>
2019-05-27 12:41:37 +0200

    Update Pressure.cpp

--- ETC/lib/Sensors/Pressure.cpp
+++ ETC/lib/Sensors/Pressure.cpp
@@ -1,1 +1,15 @@
+#include "Pressure.h"
 
+//Constructor / Destructor
+Pressure::Pressure() : I2CBase(???, ???) {}
+
+Pressure::~Pressure() {}
+
+//Public
+void Pressure::StartMesurement() {
+	I2CBase::StartMesurement(???);
+}
+
+float Pressure::GetData() {
+	
+}
