ead509d6ec44e6835ebbc406a60dc3ec7960029a
Patrik Eichmann <eipa@gmx.ch>
2019-05-27 12:41:50 +0200

    Update Pressure.h

--- ETC/lib/Sensors/Pressure.h
+++ ETC/lib/Sensors/Pressure.h
@@ -1,1 +1,14 @@
+#ifndef _PRESSURE_h
+#define _PRESSURE_h
 
+#include "I2CBase.h"
+
+class Pressure : protected I2CBase {
+public:
+	Pressure();
+	~Pressure();
+
+	void StartMesurement();
+	float GetData();
+};
+#endif
