c03520f3786af1a9fe13d59a0f765c7bae651f52
Patrik Eichmann <eipa@gmx.ch>
2019-05-28 13:40:29 +0200

    Update I2CBase.cpp

--- ETC/lib/Sensors/Base/I2CBase.cpp
+++ ETC/lib/Sensors/Base/I2CBase.cpp
@@ -2,22 +2,41 @@
 
 unsigned char mI2CAdress;
 unsigned char mRegister;
+static boolean mInitWire = true;
 
 //Constructor / Destructor
 I2CBase::I2CBase(unsigned char aI2CAdress, unsigned char aRegister) {
 	mI2CAdress = aI2CAdress;
 	mRegister = aRegister;
+	
+	if (mInitWire) {
+		mInitWire = false;
+		Wire.begin();
+	}
+	
+	Wire.onReceive(OnReceiveData);
 }
 
 I2CBase::~I2CBase() {}
 
 //Protected
-void I2CBase::StartMeasurement() {
+//0: Successful send.
+//1: Send buffer too large for the twi buffer. This should not happen, as the TWI buffer length set in twi.h is equivalent to the send buffer length set in Wire.h.
+//2: Address was sent and a NACK received. This is an issue, and the master should send a STOP condition.
+//3: Data was sent and a NACK received. This means the slave has no more to send. The master can send a STOP condition, or a repeated START.
+//4: Another twi error took place (eg, the master lost bus arbitration).
+char I2CBase::StartMeasurement(unsigned char aDataSize) {
+	char lResult;
+	
 	Wire.beginTransmission(mI2CAdress);
 	Wire.write(mRegister);
-	Wire.endTransmission();
-
-	Wire.requestFrom(mI2CAdress, mDataSize);
+	lResult = Wire.endTransmission();
+	
+	if (lResult == 0) {
+		Wire.requestFrom(mI2CAdress, aDataSize, true);
+	} else {
+		return lResult;
+	}
 }
 
 void I2CBase::SetData(unsigned char aData[]) {
