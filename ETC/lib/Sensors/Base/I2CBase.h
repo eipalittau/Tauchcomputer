68e520e857c1013d02802c20aeb836018b468e85
Patrik Eichmann <eipa@gmx.ch>
2019-05-28 14:59:20 +0200

    Update I2CBase.h

--- ETC/lib/Sensors/Base/I2CBase.h
+++ ETC/lib/Sensors/Base/I2CBase.h
@@ -7,15 +7,35 @@
 
 class I2CBase {
 protected:
+	///<summary>Constructor</summary>
+	///<param name="aI2CAdress">I2C Bus-Addresse</param>
+	///<param name="aRegister">Anzusprechendes Sensor-Register</param>
 	I2CBase(unsigned char aI2CAdress, unsigned char aRegister);
+
+	///<summyry>Destructor</summary>
 	~I2CBase();
-	
-	void StartMeasurement();
+
+	///<summary>Weisst den Sensor zur Datenerfassung an.</summary>
+	///<param name="aDataSize">Anzahl Bytes die erwartet werden.</param>
+	///<returns>0 = Successful send.
+	///1 = Send buffer too large for the twi buffer.
+	///2 = Address was sent and a NACK received. This is an issue, and the master should send a STOP condition.
+	///3: Data was sent and a NACK received. This means the slave has no more to send. The master can send a STOP condition, or a repeated START.
+	///4: Another twi error took place (eg, the master lost bus arbitration).</returns>
+	char StartMesurement(unsigned char aDataSize);
+
 	void SetData(unsigned char aData[]);
-	void GetData(unsigned char aData[]);
+
+	///<summary>Liest die gesammelten Daten des Sensors.</summary>
+	///<param name="aData">Array, in welchen die Daten geschrieben werden.</param>
+	///<returns>Wieviele Bytes gelesen wurden.</returns>
+	int GetData(unsigned char aData[]);
 
 private:
+	///<summary>Konvertiert den übergebenen dezimalen Wert in einen Hexadezimalen.</summary>
 	unsigned char Dec2Hex(unsigned char aValue);
+
+	///<summary>Konvertiert den übergebenen hexadezimalen Wert in einen Dezimalen.</summary>
 	unsigned char Hex2Dec(unsigned char aValue);
 };
 #endif
