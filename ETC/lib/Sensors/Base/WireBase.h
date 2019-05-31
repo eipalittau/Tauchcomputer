29f9e29c322722f4805429419b853684addff9f3
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 14:59:07 +0200

    Update WireBase.h

--- ETC/lib/Sensors/Base/WireBase.h
+++ ETC/lib/Sensors/Base/WireBase.h
@@ -3,16 +3,17 @@
 
 #include <OneWire.h>
 #include "Constants.h"
+#include <assert.h>
 
 class WireBase {
 protected:
-	WireBase(unsigned char aPinNumber);
+	WireBase(unsigned char aPinNumber, unsigned char aDeviceAdressSize);
 	~WireBase();
-	
+
 	void StartMesurement(unsigned char aCommand);
-  unsigned char GetData(unsigned char aCommand, unsigned char aData[]);
-	
-//Private
-  bool SendCommand(unsigned char aValue);
+	unsigned char GetData(unsigned char aCommand, unsigned short aData[]);
+
+	//Private
+	bool SendCommand(unsigned char aValue);
 };
 #endif
