316ba71482799647b8034186d93d2dbe2a29f23a
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 15:01:29 +0200

    Update Temperature.h

--- ETC/lib/Sensors/Temperature.h
+++ ETC/lib/Sensors/Temperature.h
@@ -2,7 +2,9 @@
 #define _TEMPERATURE_h
 
 #include <OneWire.h>
+#include "WireBase.h"
 #include "Constants.h"
+#include <float.h>
 
 class Temperature : protected WireBase {
 public:
@@ -10,6 +12,6 @@ public:
 	~Temperature();
 
 	void StartMesurement();
-	float GetData();
+	float GetData(UnitMesurmentTemperature aUnitMesurment = C);
 };
 #endif
