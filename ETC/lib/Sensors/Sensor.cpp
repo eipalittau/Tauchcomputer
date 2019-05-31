ef4feb64039cd6f29c0d26232c3d421a992e095c
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 15:37:47 +0200

    Update Sensor.cpp

--- ETC/lib/Sensors/Sensor.cpp
+++ ETC/lib/Sensors/Sensor.cpp
@@ -1,17 +1,35 @@
 #include "Sensor.h"
 
+Clock mClock;
+Temperature mTemperature;
+Pressure mPressure;
+
+const unsigned char INTERVALL_5 = 5;
+
 //Constructor / Destructor
 Sensor::Sensor() {}
 
 Sensor::~Sensor() {}
 
 //Public
-float Sensor::GetData() {
-  Clock lClock;
-  Temperature lTemperature;
-  Pressure lPressure;
-  
-  lClock.StartMesurement();
-  lTemperature.StartMesurement();
-  lPressure.StartMesurment();
+void Sensor::StartMesurement(unsigned char aTick) {
+	mClock.StartMesurement();
+	mPressure.StartMesurement();
+
+	if (aTick % INTERVALL_5 == 0) {
+		mTemperature.StartMesurement();
+	}
+}
+
+SensorData Sensor::GetData(unsigned char aTick) {
+	SensorData lData;
+
+	lData.DateTime = mClock.GetData();
+	lData.Pressure = mPressure.GetData();
+
+	if (aTick % INTERVALL_5 == 0) {
+		lData.Temperature = mTemperature.GetData(Temperature::C);
+	}
+
+	return lData;
 }
