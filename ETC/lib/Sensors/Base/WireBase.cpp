cd48a60bebfb4f42f50b0efd67bf9c888291199f
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 14:59:26 +0200

    Update WireBase.cpp

--- ETC/lib/Sensors/Base/WireBase.cpp
+++ ETC/lib/Sensors/Base/WireBase.cpp
@@ -1,47 +1,63 @@
 #include "WireBase.h"
 
-OneWire m1Wire;
+OneWire* m1Wire;
+uint8_t* mDeviceAddress;
 
-//Constructor / Destructor
-WireBase::WireBase(unsigned char aPinNumber) {
-	m1Wire(aPinNumber);
+#pragma region Constructor / Destructor
+WireBase::WireBase(unsigned char aPinNumber, const unsigned char aDeviceAdressSize) {
+	assert(aPinNumber < 0 || aPinNumber > 13);
+
+	m1Wire = new OneWire(aPinNumber);
+	mDeviceAddress = new uint8_t[aDeviceAdressSize]();
+
+	while (m1Wire->search(mDeviceAddress)) {
+		if (m1Wire->crc8(mDeviceAddress, aDeviceAdressSize - 1) == mDeviceAddress[aDeviceAdressSize - 1]) {
+			break;
+		}
+	}
 }
 
-WireBase::~WireBase() {}
+WireBase::~WireBase() {
+	assert(m1Wire == NULL);
 
-//Protected
+	delete m1Wire;
+}
+#pragma endregion
+
+#pragma region Protected
 void WireBase::StartMesurement(unsigned char aCommand) {
-	SendCommand(aValue);
+	SendCommand(aCommand);
 }
 
 unsigned char WireBase::GetData(unsigned char aCommand, unsigned short aData[]) {
 	unsigned long lStart = millis();
 
-	while (m1Wire->read_bit() != 1 && millis() - lStart < Constants.TIMEOUT) {
-		delay(2)
+	while (m1Wire->read_bit() != 1 && millis() - lStart < TIMEOUT) {
+		delay(2);
 	}
-	
+
 	if (SendCommand(aCommand)) {
 		unsigned short lSize = sizeof(aData) / sizeof(unsigned char);
-		
+
 		for (unsigned char lI = 0; lI < lSize; lI++) {
-			aData[i] = m1Wire->read();
+			aData[lI] = m1Wire->read();
 		}
-		
+
 		return 0;
 	}
-	
+
 	return 1;
 }
+#pragma endregion
 
 //Private
 bool WireBase::SendCommand(unsigned char aValue) {
 	if (m1Wire->reset() == 0) {
 		return false;
 	}
-	
-	m1Wire->select(deviceAddress);
+
+	m1Wire->select(mDeviceAddress);
 	m1Wire->write(aValue);
-	
+
 	return true;
 }
