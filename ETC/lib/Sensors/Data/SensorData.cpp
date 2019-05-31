7ad3c4441c3d4b329f9d8ad17cbd6f9d113f1edb
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 15:41:49 +0200

    Update SensorData.cpp

--- ETC/lib/Sensors/Data/SensorData.cpp
+++ ETC/lib/Sensors/Data/SensorData.cpp
@@ -5,15 +5,15 @@ SensorData::SensorData() {}
 
 SensorData::~SensorData() {}
 
-DateTimeData _DateTime;
-void SensorData::DateTime(DateTimeData aValue) {
+DateTimeData* _DateTime = NULL;
+void SensorData::DateTime(DateTimeData* aValue) {
 	_DateTime = aValue;
 }
-DateTimeData SensorData::DateTime() {
+DateTimeData* SensorData::DateTime() {
 	return _DateTime;
 }
 
-float _Temperature;
+float _Temperature = FLOAT_MIN;
 void SensorData::Temperature(float aValue) {
 	_Temperature = aValue;
 }
@@ -21,7 +21,7 @@ float SensorData::Temperature() {
 	return _Temperature;
 }
 
-float _Pressure;
+float _Pressure = FLOAT_MIN;
 void SensorData::Pressure(float aValue) {
 	_Pressure = aValue;
 }
