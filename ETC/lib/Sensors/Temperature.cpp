ad007d9f3ee810e0fd671504feade31509812762
Patrik Eichmann <eipa@gmx.ch>
2019-05-31 15:01:58 +0200

    Update Temperature.cpp

--- ETC/lib/Sensors/Temperature.cpp
+++ ETC/lib/Sensors/Temperature.cpp
@@ -1,7 +1,9 @@
 #include "Temperature.h"
 
+enum UnitMesurmentTemperature { C, F, K };
+
 //Constructor / Destructor
-Temperature::Temperature() : WireBase(2) {}
+Temperature::Temperature() : WireBase(2, 8) {}
 
 Temperature::~Temperature() {}
 
@@ -10,12 +12,28 @@ void Temperature::StartMesurement() {
 	WireBase::StartMesurement(0x44);
 }
 
-float Temperature::GetData() {
-	signed short lData[9]
+float Temperature::GetData(UnitMesurmentTemperature aUnitMesurment) {
+	unsigned short lData[9];
+	float lResult = FLT_MIN;
+
+	if (WireBase::GetData(0xBE, lData) == 0) {
+		lResult = (float)((lData[1] << 11) | (lData[0] << 3));
 		
-	if (WireBase::GetData(0xBE, lData) = 0) {
-		return (float) ((lData[1] << 11) | (lData[0] << 3)) * 0.0078125;
-	} else {
-		return numeric_limits<float>::min();
+		switch (aUnitMesurment) {
+		case F:
+			lResult = lResult * 0.0140625 + 32;
+			break;
+
+		case K:
+			lResult = lResult * 0.0078125 + 273.15;
+			break;
+
+		case C:
+		default:
+			lResult = lResult * 0.0078125;
+			break;
+		}
 	}
+
+	return lResult;
 }
