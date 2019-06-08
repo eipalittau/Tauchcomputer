79db469761068051f61667444fe71663a3cda7ee
Patrik Eichmann <eipa@gmx.ch>
2019-06-08 18:14:35 +0200

    Update WireBase.h

--- ETC/lib/Sensors/Base/WireBase.h
+++ ETC/lib/Sensors/Base/WireBase.h
@@ -7,13 +7,25 @@
 
 class WireBase {
 protected:
+	///<summary>Constructor</summary>
+	///<param name="aPinNumber">Nummer des angeschlossenen Pin's.</param>
+	///<param name="aDeviceAdressSize">Grösse des Arrays.</param>
 	WireBase(unsigned char aPinNumber, unsigned char aDeviceAdressSize);
+
+	///<summyry>Destructor</summary>
 	~WireBase();
 
+	///<summary>Weisst den Sensor zur Datenerfassung an.</summary>
+	///<param name="aCommand">Anzusprechendes Sensor-Register</param>
 	void StartMesurement(unsigned char aCommand);
+
+	///<summary>Liest die gesammelten Daten des Sensors.</summary>
+	///<param name="aCommand">Anzusprechendes Sensor-Register.</param>
+	///<param name="aData">Array, in welchen die Daten geschrieben werden.</param>
+	///<returns</returns>
 	unsigned char GetData(unsigned char aCommand, unsigned short aData[]);
 
-	//Private
+private:
 	bool SendCommand(unsigned char aValue);
 };
 #endif
