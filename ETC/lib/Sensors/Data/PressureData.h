1dcc8aca0c174fb1be98c5c84c3edd5006c448f4
Patrik Eichmann <eipa@gmx.ch>
2019-06-17 16:47:30 +0200

    Update PressureData.h

--- ETC/lib/Sensors/Data/PressureData.h
+++ ETC/lib/Sensors/Data/PressureData.h
@@ -5,20 +5,20 @@ class PressureData {
 public:
 	PressureData();
 	~PressureData();
-
-  int64_t Sensitivity();
-  int64_t Offset();
-  int64_t TCS();
-  int64_t TCO();
-  uint32_t ReferenceTemperature();
-  int64_t TCT();
+	
+	int64_t Sensitivity();
+	int64_t Offset();
+	int64_t TCS();
+	int64_t TCO();
+	uint32_t ReferenceTemperature();
+	int64_t TCT();
   
 private:
-  int64_t _Sensitivity;
-  int64_t _Offset;
-  int64_t _TCS;
-  int64_t _TCO;
-  uint32_t _ReferenceTemperature;
-  int64_t _TCT;
+	int64_t _Sensitivity;
+	int64_t _Offset;
+	int64_t _TCS;
+	int64_t _TCO;
+	uint32_t _ReferenceTemperature;
+	int64_t _TCT;
 };
 #endif
