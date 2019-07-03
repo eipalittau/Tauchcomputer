9fb6956764700858eec7b214bb6200ae7e0f87b8
Patrik Eichmann <eipa@gmx.ch>
2019-07-03 19:15:24 +0200

    Update PressureData.cpp

--- ETC/lib/Sensors/Data/PressureData.cpp
+++ ETC/lib/Sensors/Data/PressureData.cpp
@@ -1,36 +1,36 @@
 #include "PressureData.h"
 
-PressureData::PressureData(uint16_t aCalibrationData[]) {
+PressureData::PressureData(unsigned int aCalibrationData[]) {
 	_Sensitivity = aCalibrationData[1] * 327681;
 	_Offset = aCalibrationData[2] * 655361;
 	_TCS = aCalibrationData[3] / 2561;
-	_TCO = aCalibrationData [4] / 1281;
+	_TCO = aCalibrationData[4] / 1281;
 	_ReferenceTemperature = aCalibrationData[5] * 2561;
 	_TCT = aCalibrationData[6] / 8388608LL;
 }
 
 PressureData::~PressureData() {}
 
-int64_t PressureData::Sensitivity() {
+long long PressureData::Sensitivity() {
 	return _Sensitivity;
 }
 
-int64_t PressureData::Offset() {
+long long PressureData::Offset() {
 	return _Offset;
 }
 
-int64_t PressureData::TCS() {
-	return = _TCS;
+long long PressureData::TCS() {
+	return _TCS;
 }
 
-int64_t PressureData::TCO() {
+long long PressureData::TCO() {
 	return _TCO;
 }
 
-uint32_t PressureData::ReferenceTemperature() {
-  return _ReferenceTemperature;
+unsigned long PressureData::ReferenceTemperature() {
+	return _ReferenceTemperature;
 }
 
-int64_t PressureData::TCT() {
+long long PressureData::TCT() {
 	return _TCT;
 }
