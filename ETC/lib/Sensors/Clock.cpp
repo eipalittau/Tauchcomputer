a2eaa7b2c2fcf2d844c36bc069fbfea4da5957eb
Patrik Eichmann <eipa@gmx.ch>
2019-07-03 18:13:48 +0200

    Update Clock.cpp

--- ETC/lib/Sensors/Clock.cpp
+++ ETC/lib/Sensors/Clock.cpp
@@ -1,6 +1,6 @@
 #include "Clock.h"
 
-const unsigned char ARRAYSIZE = 7;
+const uint8_t ARRAYSIZE = 7;
 
 #pragma region Constructor / Destructor
 Clock::Clock() : I2CBase(0x68) {}
@@ -10,19 +10,19 @@ Clock::~Clock() {}
 
 #pragma region Public
 void Clock::StartMesurement() {
-	unsigned char lSize = ARRAYSIZE;
+	uint8_t lSize = ARRAYSIZE;
 
 	I2CBase::StartMesurement(0x00, lSize);
 }
 
 void Clock::SetData(DateTimeData aData) {
-	unsigned char lTimestamp[ARRAYSIZE] = { aData.Second(), aData.Minute(), aData.Hour(), aData.Weekday(), aData.Day(), aData.Month(), aData.Year() - 2000 };
+	uint8_t lTimestamp[ARRAYSIZE] = { aData.Second(), aData.Minute(), aData.Hour(), aData.Weekday(), aData.Day(), aData.Month(), aData.Year() - 2000 };
 
 	I2CBase::SetData(lTimestamp);
 }
 
 DateTimeData Clock::GetData() {
-	unsigned char lData[ARRAYSIZE];
+	uint8_t lData[ARRAYSIZE];
 	DateTimeData lResult;
 
 	if (I2CBase::GetData(lData) >= 7) {
