4eb5f0b5661b6a50978294abf64ddf6dee74ebcc
Patrik Eichmann <eipa@gmx.ch>
2019-07-03 18:40:42 +0200

    Update Pressure.h

--- ETC/lib/Sensors/Pressure.h
+++ ETC/lib/Sensors/Pressure.h
@@ -2,24 +2,24 @@
 #define _PRESSURE_h
 
 #include "I2CBase.h"
-#include "PressureData.h"
-#include <math.h>
-#include <assert.h>
 
 class Pressure : protected I2CBase {
 public:
 	Pressure();
 	~Pressure();
 
-	bool IsCrcOk();
-	int32_t GetData();
+	bool StartMesurement();
+	float GetData();
 
 private:
-	uint32_t ReadData(uint8_t aRegister);
-	uint8_t CheckCrc(uint16_t n_prom[]);
-	
-	bool _IsCrcOk = false;
-	uint32_t mNextAction = 0;
-	PressureData* mPressureData;	
+	unsigned int C[8];
+	unsigned long D1, D2;
+	long P;
+
+
+	bool ReadTemperature();
+	unsigned long ReadPressure(unsigned char aRegister);
+	unsigned char crc4(unsigned int n_prom[]);
+	void Calculate();
 };
 #endif
