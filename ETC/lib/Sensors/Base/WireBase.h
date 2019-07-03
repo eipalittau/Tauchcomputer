c01a1c64c568b3cc0d9a016ebd3ad7072dae2273
Patrik Eichmann <eipa@gmx.ch>
2019-07-03 18:11:46 +0200

    Update WireBase.h

--- ETC/lib/Sensors/Base/WireBase.h
+++ ETC/lib/Sensors/Base/WireBase.h
@@ -10,23 +10,23 @@ protected:
 	///<summary>Constructor</summary>
 	///<param name="aPinNumber">Nummer des angeschlossenen Pin's.</param>
 	///<param name="aDeviceAdressSize">Grösse des Arrays.</param>
-	WireBase(unsigned char aPinNumber, unsigned char aDeviceAdressSize);
+	WireBase(uint8_t aPinNumber, uint8_t aDeviceAdressSize);
 
 	///<summyry>Destructor</summary>
 	~WireBase();
 
 	///<summary>Weisst den Sensor zur Datenerfassung an.</summary>
 	///<param name="aCommand">Anzusprechendes Sensor-Register</param>
-	void StartMesurement(unsigned char aCommand);
+	void StartMesurement(uint8_t aCommand);
 
 	///<summary>Liest die gesammelten Daten des Sensors.</summary>
 	///<param name="aCommand">Anzusprechendes Sensor-Register.</param>
 	///<param name="aData">Array, in welchen die Daten geschrieben werden.</param>
 	///<returns</returns>
-	unsigned char GetData(unsigned char aCommand, unsigned short aData[]);
+	unsigned char GetData(uint8_t aCommand, uint16_t aData[]);
 
 private:
-	bool SendCommand(unsigned char aValue);
+	bool SendCommand(uint8_t aValue);
 
 	OneWire* m1Wire;
 	uint8_t* mDeviceAddress;
