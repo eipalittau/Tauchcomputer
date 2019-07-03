e22f2886049cef8a06665f17a0b8e742b0310101
Patrik Eichmann <eipa@gmx.ch>
2019-07-03 18:41:15 +0200

    Update Temperature.h

--- ETC/lib/Sensors/Temperature.h
+++ ETC/lib/Sensors/Temperature.h
@@ -1,17 +1,13 @@
 #ifndef _TEMPERATURE_h
 #define _TEMPERATURE_h
 
-#include <OneWire.h>
 #include "WireBase.h"
 #include "Constants.h"
-#include "/Projekte/ETC/ETC/lib/Settings.h"
 
 class Temperature : protected WireBase {
 public:
 	Temperature();
 	~Temperature();
-
-	enum TemperatureUnitEnum { C, F, K };
 
 	void StartMesurement();
 	float GetData();
