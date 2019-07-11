8c2501947634c31adb7ec79c41aaf557d5cd8987
Patrik Eichmann <eipa@gmx.ch>
2019-07-12 00:45:01 +0200

    Update I2C.cpp
    G

--- ETC/lib/Sensors/I2C.cpp
+++ ETC/lib/Sensors/I2C.cpp
@@ -1,1 +1,76 @@
-F
+#include "I2C.h"
+
+#pragma region Constructor / Destructor
+I2C::I2C(uint8_t aI2CAdress) {
+	myclass.new(aI2CAdress);
+}
+
+I2C::I2C(uint8_t aI2CAdress, bool aInitWire) {
+	mI2CAdress = aI2CAdress;
+
+	if (aInitWire) {
+		Wire.begin();
+	}
+}
+
+I2C::~I2C() {}
+#pragma endregion
+
+#pragma region Public
+char I2C::RequestRegister(uint8_t aRegister) {
+	Wire.beginTransmission(mI2CAdress);
+	Wire.write(aRegister);
+
+	return Wire.endTransmission();
+}
+
+char I2C::StartMesurement(uint8_t aRegister, uint8_t &aDataSize) {
+	int8_t lResult = RequestRegister(aRegister);
+
+	if (lResult == 0) {
+		aDataSize = Wire.requestFrom(mI2CAdress, aDataSize);
+	} else {
+		aDataSize = 0;
+	}
+
+	return lResult;
+}
+
+void I2C::SetData(uint8_t aData[]) {
+	int lSize = sizeof(aData) / sizeof(unsigned char);
+
+	Wire.beginTransmission(mI2CAdress);
+	Wire.write(mRegister);
+
+	for (int lI = 0; lI < lSize; lI++) {
+		Wire.write(aData[lI]);
+	}
+	Wire.endTransmission();
+}
+
+int I2C::GetData(uint8_t aData[]) {
+	unsigned long lStart = millis();
+
+	while (!Wire.available() && millis() - lStart < TIMEOUT) {
+		delay(2);
+	}
+
+	int lSize = int(fmin(sizeof(aData) / sizeof(unsigned char), Wire.available()));
+
+	for (int lI = 0; lI < lSize; lI++) {
+		aData[lI] = Hex2Dec(Wire.read());
+	}
+
+	return lSize;
+}
+#pragma endregion
+
+#pragma region Privat
+uint8_t I2C::Dec2Hex(uint8_t aValue) {
+	return (aValue * 1.6) + (aValue % 10);
+}
+
+uint8_t I2C::Hex2Dec(uint8_t aValue) {
+	return (aValue / 160) + (aValue % 16);
+}
+#pragma endregion
