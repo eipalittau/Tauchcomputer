31ce450b0c5b59ee2823c3248413c97872bb88bc
Patrik Eichmann <eipa@gmx.ch>
2019-07-16 15:37:50 +0200

    Update Wire.cpp

--- ETC/lib/Sensors/Wire.cpp
+++ ETC/lib/Sensors/Wire.cpp
@@ -1,4 +1,10 @@
-m1Wire = new OneWire(aPinNumber);
+#include "Wire.h"
+
+#pragma region Constructor / Destructor
+Wire::Wire(uint8_t aPinNumber, const uint8_t aDeviceAdressSize) {
+	assert(aPinNumber < 0 || aPinNumber > 13);
+
+	m1Wire = new OneWire(aPinNumber);
 	mDeviceAddress = new uint8_t[aDeviceAdressSize]();
 
 	while (m1Wire->search(mDeviceAddress)) {
@@ -6,3 +12,50 @@ m1Wire = new OneWire(aPinNumber);
 			break;
 		}
 	}
+}
+
+Wire::~Wire() {
+	assert(m1Wire == NULL);
+
+	delete m1Wire;
+}
+#pragma endregion
+
+#pragma region Protected
+void Wire::StartMesurement(unsigned char aCommand) {
+	SendCommand(aCommand);
+}
+
+unsigned char WireBase::GetData(unsigned char aCommand, uint16_t aData[]) {
+	uint32_t lStart = millis();
+
+	while (m1Wire->read_bit() != 1 && millis() - lStart < TIMEOUT) {
+		delay(2);
+	}
+
+	if (SendCommand(aCommand)) {
+		uint16_t lSize = sizeof(aData) / sizeof(unsigned char);
+
+		for (unsigned char lI = 0; lI < lSize; lI++) {
+			aData[lI] = m1Wire->read();
+		}
+
+		return 0;
+	}
+
+	return 1;
+}
+#pragma endregion
+
+#pragma region Private
+bool Wire::SendCommand(uint8_t aValue) {
+	if (m1Wire->reset() == 0) {
+		return false;
+	}
+
+	m1Wire->select(mDeviceAddress);
+	m1Wire->write(aValue);
+
+	return true;
+}
+#pragma endregion
