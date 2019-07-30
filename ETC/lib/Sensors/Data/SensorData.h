dd8f6c3fe61b93bd013d943dfcaf1f124acfdeb1
Patrik Eichmann <eipa@gmx.ch>
2019-07-30 20:12:48 +0200

    Update SensorData.h

--- ETC/lib/Sensors/Data/SensorData.h
+++ ETC/lib/Sensors/Data/SensorData.h
@@ -1,44 +1,20 @@
 #ifndef _SENSORDATA_h
 #define _SENSORDATA_h
 
-#include "Essentials.h"
+typedef struct DateTimeStructDef {
+	unsigned char Second;
+	unsigned char Minute;
+	unsigned char Hour;
+	unsigned char Day;
+	unsigned char Month;
+	unsigned short int Year;
+	unsigned char Weekday;
+} DateTimeStruct;
 
-class SensorData {
-public:
-	SensorData();
-	~SensorData();
-
-	DateTimeData* DateTime();
-	void Temperature(float aValue);
-	float Temperature();
-	void Pressure(float aValue);
+typedef struct SensorStructDef {
+	DateTimeStruct DateTime;	
+	float Temperature;
 	float Pressure();
+} SensorStruct;
 
-class DateTimeData {
-public:
-	unsigned char Second();
-	void Second(unsigned char aValue);
-	unsigned char Minute();
-	void Minute(unsigned char aValue);
-	unsigned char Hour();
-	void Hour(unsigned char aValue);
-	unsigned char Day();
-	void Day(unsigned char aValue);
-	unsigned char Month();
-	void Month(unsigned char aValue);
-	unsigned short int Year();
-	void Year(unsigned short int aValue);
-	unsigned char Weekday();
-	void Weekday(unsigned char aValue);
-
-private:
-	DateTimeData();
-	~DateTimeData();
-};
-
-Private:
-	DateTimeData* _DateTime = new DateTimeData();
-	float _Temperature = FLOAT_MIN;
-	float _Pressure = FLOAT_MIN;
-};
 #endif
