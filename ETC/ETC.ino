f1cf25120a32fc568a3163c87e44082a9d01898f
Patrik Eichmann <eipa@gmx.ch>
2019-07-30 20:26:10 +0200

    Update ETC.ino

--- ETC/ETC.ino
+++ ETC/ETC.ino
@@ -1,35 +1,16 @@
-// Visual Micro is in vMicro>General>Tutorial Mode
-// 
-/*
-    Name:       ETC.ino
-    Created:	15.05.2019 18:59:49
-    Author:     TOW-U114L-MAIN\eipa
-*/
+#include "SensorData.h"
+#include "Sensors.h"
 
-// Define User Types below here or use a .h file
-//
+Sensors mSensors;
+SensorStruct mSensorData;
 
-
-// Define Function Prototypes that use User Types below here or use a .h file
-//
-
-
-// Define Functions below here or use other .ino or cpp files
-//
-
-// The setup() function runs once each time the micro-controller starts
-#include "I2CBase.h"
-#include <Wire.h>
-
-void setup()
-{
-
-
+void setup() {
+    
 }
 
-// Add the main program code into the continuous loop() function
-void loop()
-{
-
-
+void loop() {
+    mSensors.StartMesurement();
+    mSensors.GetData(&mSensorData);
+    
+    
 }
