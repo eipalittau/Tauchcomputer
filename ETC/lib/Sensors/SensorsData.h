c9e40e62a454a30a1e2c8498f330e754ea2715c2
Patrik Eichmann <eipa@gmx.ch>
2019-08-10 18:09:41 +0200

    Create SensorsData.h

---
+++ ETC/lib/Sensors/SensorsData.h
@@ -0,0 +1,19 @@
+#ifndef _SENSORDATA_h
+#define _SENSORDATA_h
+
+typedef struct DateTimeStructDef {
+  unsigned char Second;
+  unsigned char Minute;
+  unsigned char Hour;
+  unsigned char Day;
+  unsigned char Month;
+  unsigned short int Year;
+  unsigned char Weekday;
+} DateTimeStruct;
+
+typedef struct SensorStructDef {
+  DateTimeStruct DateTime;  
+  float Temperature;
+  float Pressure();
+} SensorStruct;
+#endif
