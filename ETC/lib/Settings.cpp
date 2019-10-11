27a9db92b55a00e92b405e383f8cbea154b56c0f
Patrik Eichmann <eipa@gmx.ch>
2019-10-11 21:32:17 +0200

    Update Settings.cpp

--- ETC/lib/Settings.cpp
+++ ETC/lib/Settings.cpp
@@ -10,6 +10,22 @@ void static Settings::SetTemperatureUnit(TemperatureUnitEnum aValue) {
 	_TemperatureUnit = aValue;
 }
 
-Settings::TemperatureUnitEnum static Settings::GetTemperatureUnit() {
+TemperatureUnitEnum static Settings::GetTemperatureUnit() {
 	return _TemperatureUnit;
+}
+
+void static Settings::SetPressureUnit(PressureUnitEnum aValue) {
+	_PressureUnit = aValue;
+} 
+
+PressureUnitEnum static Settings::GetPressureUnit() {
+	return _PressureUnit;
+}
+
+void static Settings::SetWaterDensity(int aValue) {
+	_WaterDensity = aValue;
+}
+
+int static Settings::GetWaterDensity() {
+	return _WaterDensity;
 }
