0873634106e2c1ebe34605074226e1b65ebe9e5b
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 18:34:13 +0200

    Create GasBase.cs

---
+++ New/GasBase.cs
@@ -0,0 +1,7 @@
+namespace ETC.Buehlmann {
+  private protected GasBase {
+    private protected PI(float pAmbientPressure, float pGasFraction) {
+      return (pAmbientPressure - Constant.)
+    }
+  }
+}
