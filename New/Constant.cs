08026f42bf9fad48f8f474339a89df8198e3399c
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 18:27:39 +0200

    Create Constant.cs

---
+++ New/Constant.cs
@@ -0,0 +1,9 @@
+namespace ETC {
+  public static class Constant {
+    <summary>Respiratorischer Quotient</summary>
+    public float RQ { get; } = 0.8;
+
+    <summary>Wasserdampfdruck</summary>
+    public float PH2O { get; } = 0.0627;
+  }
+}
