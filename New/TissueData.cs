909fce4cfd61241b22206c7aaff5b0058783833e
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 17:58:04 +0200

    Create TissueData.cs

---
+++ New/TissueData.cs
@@ -0,0 +1,12 @@
+namespace ETC.Buehlmann {
+  public sealed class TissueData {
+    ///<summary>Halbwertzeit des Kompartiments</summary>
+    public float Halflife { get; set; }
+
+    ///<summary>Koeffizient a</summary>
+    public float A { get; set; }
+
+    ///<summary>Koeffizient b</summary>
+    public float B { get; set; }
+  }
+}
