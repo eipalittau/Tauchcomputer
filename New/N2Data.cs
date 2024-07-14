eb569641de81a582b975830c9d14717c7cc08546
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 18:48:24 +0200

    Update N2Data.cs

--- New/N2Data.cs
+++ New/N2Data.cs
@@ -1,14 +1,20 @@
 namespace ETC.Buehlmann {
-  public static class N2Data {
+  public class N2Data : GasData {
     <summary>Nur N2=0.78084; N2+Ar+Ne=0.79020</summary>
-    public float Fraction { get; } = 0.79020;
+    public static float Fraction { get; } = 0.79020;
 
-    public TissueData[] Tissues { get; }
+    public static TissueData[] Tissues { get; }
 
     static N2Data() {
       Tissues = {
         new TissueData() { Halftime: 1, A: 1, B: 1 }
       }
+    }
+
+    public N2Data() {}
+
+    public float PIN2(float pAmbientPressure) {
+      return base.PI(pAmbientPressure, Fraction);
     }
   }
 }
