5bfb9a41e0c74f63563b4dde954c4d909a4d6cf4
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 18:17:17 +0200

    Create HeData.cs

---
+++ New/HeData.cs
@@ -0,0 +1,13 @@
+namespace ETC.Buehlmann {
+  public static class HeData {
+    public float Fraction { get; } = 0.000005;
+
+    public TissueData[] Tissues { get; }
+
+    static N2Data() {
+      Tissues = {
+        new TissueData() { Halftime: 1, A: 1, B: 1 }
+      }
+    }
+  }
+}
