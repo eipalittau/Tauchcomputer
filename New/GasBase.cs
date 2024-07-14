e4c296f83b50a3e528e2a793f0c5ae76f4d55725
Patrik Eichmann <eipa@gmx.ch>
2024-07-14 18:50:53 +0200

    Update GasBase.cs

--- New/GasBase.cs
+++ New/GasBase.cs
@@ -1,10 +1,14 @@
 namespace ETC.Buehlmann {
   public abstract GasBase {
-    private protected GasBase() {}
+    public float Fraction { get; private set; };
+
+    private protected GasBase(float pFraction) {
+      Fraction = pFraction;
+    }
     
     <summary>Partialdruck im Atemgas bei der Einatmung</summary>
-    private protected PI(float pAmbientPressure, float pGasFraction) {
-      return (pAmbientPressure - Constant.PH2O) * pGasFraction;
+    private protected PI(float pAmbientPressure) {
+      return (pAmbientPressure - Constant.PH2O) * Fraction;
     }
   }
 }
