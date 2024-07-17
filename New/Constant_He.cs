eeb1b0bf37b832f3545c973ff889ccf44c049765
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 14:30:40 +0200

    Update Constant_He.cs

--- New/Constant_He.cs
+++ New/Constant_He.cs
@@ -2,7 +2,7 @@ namespace ETC.Buehlmann {
   public static partial class Constant {
     public static class He {
       <summary></summary>
-      public static float BaseFraction { get; } = 0.79020;
+      public static float BaseFraction { get; } = 0.000005;
       
       public static CompartmentData[] Compartment { get; }
     }
