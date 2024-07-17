30681a802497956ad43bacdf795b10f9c6d55528
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 14:24:49 +0200

    Create Constant_N2.cs

---
+++ New/Constant_N2.cs
@@ -0,0 +1,9 @@
+namespace ETC.Buehlmann {
+  public static partial class Constant {
+    public static class N2 {
+      public static float BaseFraction { get; } = 0.78;
+      
+      public static CompartmentData[] Compartment { get; }
+    }
+  }
+}
