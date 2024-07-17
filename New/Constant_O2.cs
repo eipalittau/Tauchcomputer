e9a9189bb5343ed6122f0fff41961734205d9c74
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 14:34:06 +0200

    Create Constant_O2.cs

---
+++ New/Constant_O2.cs
@@ -0,0 +1,10 @@
+namespace ETC.Buehlmann {
+  public static partial class Constant {
+    public static class O2 {
+      <summary>Gas-Partialdruck bei 1 bar</summary>
+      public static float BaseFraction { get; } = 0.20948;
+      
+      public static CompartmentData[] Compartment { get; }
+    }
+  }
+}
