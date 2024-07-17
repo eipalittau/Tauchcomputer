92fc9e041a27ff2180726318778b53c378760715
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 16:35:22 +0200

    Update Calculation.cs

--- New/Calculation.cs
+++ New/Calculation.cs
@@ -1,6 +1,17 @@
 namespace ETC.Buehlmann {
   public partial class Calculation {
-    public void SetAmbientPressure
+    private float _AmbientPressure;
+
+    private Calculation.N2 InertGas
+
+    public Calculation(float pAmbientPressure) {
+      SetAmbientPressure(pAmbientPressure);
+    }
+    
+    public void SetAmbientPressure(float pAmbientPressure) {
+      _AmbientPressure = pAmbientPressure;
+    }
+    
     <summary>Partialdruck im Atemgas bei der Einatmung</summary>
     private protected PI(float pAmbientPressure) {
       return (pAmbientPressure - Constant.PH2O) * BaseFraction;
