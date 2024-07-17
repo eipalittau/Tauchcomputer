1cf6a205c528aa29e9d6c031a7e64e0fa67f6a8f
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 14:09:24 +0200

    Update CompartmentData.cs

--- New/CompartmentData.cs
+++ New/CompartmentData.cs
@@ -1,1 +1,12 @@
+namespace ETC.Buehlmann {
+  public sealed class CompartmentData {
+    ///<summary>Halbwertzeit des Kompartiments</summary>
+    public float Halftime { get; set; }
 
+    ///<summary>Koeffizient a</summary>
+    public float A { get; set; }
+
+    ///<summary>Koeffizient b</summary>
+    public float B { get; set; }
+  }
+}
