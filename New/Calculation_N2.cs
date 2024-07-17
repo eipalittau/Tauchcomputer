76c6e8ffba567a171c01f15acef657c1cf6a214f
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 17:00:19 +0200

    Update Calculation_N2.cs

--- New/Calculation_N2.cs
+++ New/Calculation_N2.cs
@@ -1,9 +1,8 @@
 namespace ETC.Buehlmann {
   public class Calculation {
     public class N2 : InertGasBase, IInertGas {
-      public float Fraction { get; internal set; }
-
-      private N2(Calculation pParent) : base(pParent) { }
+      private N2(Calculation pParent)
+        : base(pParent) { }
       
       <summary>Partialdruck im Atemgas bei der Einatmung</summary>
       public float CalculateInspiredPartialPressure() {
