be400add489cdbf93e7b7595008033557298b046
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 14:32:47 +0200

    Update Constant_N2.cs

--- New/Constant_N2.cs
+++ New/Constant_N2.cs
@@ -1,7 +1,7 @@
 namespace ETC.Buehlmann {
   public static partial class Constant {
     public static class N2 {
-      <summary>Nur N2=0.78084; N2+Ar+Ne=0.79020</summary>
+      <summary>Gas-Partialdruck bei 1 bar. Nur N2=0.78084; N2+Ar+Ne=0.79020</summary>
       public static float BaseFraction { get; } = 0.79020;
       
       public static CompartmentData[] Compartment { get; }
