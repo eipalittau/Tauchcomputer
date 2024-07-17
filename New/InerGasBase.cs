211eec185330ff8d50e4734a19cec5c6eebe20e7
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 16:52:13 +0200

    Create InerGasBase.cs

---
+++ New/InerGasBase.cs
@@ -0,0 +1,14 @@
+namespace ETC.Buehlmann {
+  public abstract class InerGasBase {
+    private protected Calculation _Parent;
+
+    private protected InerGasBase(Calculation pParent) {
+      _Parent = pParent;
+    }
+      
+    <summary>Partialdruck im Atemgas bei der Einatmung</summary>
+    private protected float CalculateInspiredPartialPressure(float pBaseFraction) {
+      return (_Parent.AmbientPressure - Constant.PH2O) * pBaseFraction;
+    }
+  }
+}
