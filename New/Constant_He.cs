bb4d0cdec02d2428127b39d9deb927bf2c248c49
Patrik Eichmann <eipa@gmx.ch>
2024-07-17 17:15:28 +0200

    Update Constant_He.cs

--- New/Constant_He.cs
+++ New/Constant_He.cs
@@ -2,7 +2,7 @@ namespace ETC.Buehlmann {
   public static partial class Constant {
     public static class He {
       <summary>Gas-Partialdruck bei 1 bar</summary>
-      public static float BaseFraction { get; } = 0.000005;
+      public static float StandardGasFraction { get; } = 0.000005;
       
       public static CompartmentData[] Compartment { get; }
     }
