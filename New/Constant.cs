12ff72e93c244464213f4694ee95f0e06d9c89f5
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 17:33:54 +0200

    Update Constant.cs

--- New/Constant.cs
+++ New/Constant.cs
@@ -1,9 +1,9 @@
 namespace ETC.Buehlmann {
   public static partial class Constant {
-    <summary>Respiratorischer Quotient</summary>
-    public static float RQ { get; } = 0.8;
+    ///<summary>Respiratorischer Quotient</summary>
+    public static double RQ { get; } = 0.8;
 
-    <summary>Wasserdampfdruck</summary>
-    public static float WaterVaporPressure { get; } = 0.0627;
+    ///<summary>Wasserdampfdruck</summary>
+    public static double WaterVaporPressure { get; } = 0.0627;
   }
 }
