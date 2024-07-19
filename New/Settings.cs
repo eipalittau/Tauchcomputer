08d08d484903cff13a2cc612a93dd4a305fa1474
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 17:19:04 +0200

    Update Settings.cs

--- New/Settings.cs
+++ New/Settings.cs
@@ -8,10 +8,13 @@ namespace ETC {
 
     public float MaxPpO2Dive { get; set; }
 
+    public double MaxPpN2 { get; set; }
+
     static Settings() {
       Mixtures = new List<MixtureData>();
       MinPpO2 = 0.16;
       MaxPpO2Deco = 1.6;
       MaxPpO2Dive = 1.4;
+      MaxPpN2 = 3;
   }
 }
