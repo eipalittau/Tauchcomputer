b89acd21975e98b810ebf7b8a61187a58e7da17f
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 11:53:31 +0200

    Update MixtureTypeEnum.cs

--- New/MixtureTypeEnum.cs
+++ New/MixtureTypeEnum.cs
@@ -1,7 +1,15 @@
 namespace ETC.Buehlmann {
-  public enum MixtureTypeEnum {
-    Surfacegas,
-    Travelgas,
-    Decogas
+  public sealed class MixtureTypeEnum : EnumBase {
+    public static readonly Surfacegas = new MixtureTypeEnum(nameof(Surfacegas), false);
+    
+    public static readonly Travelgas = new MixtureTypeEnum(nameof(Travelgas), true);
+    
+    public static readonly Decogas = new MixtureTypeEnum(nameof(Decogas), true);
+
+    public bool IsSelectable { get; init; }
+    
+    private MixtureTypeEnum(string pName, bool pIsSelectable)
+      : base(pName) {
+      IsSelectable = pIsSelectable;
   }
 }
