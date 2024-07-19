59149391533789e39924311d8a900e07bb83befa
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 17:12:26 +0200

    Update Settings.cs

--- New/Settings.cs
+++ New/Settings.cs
@@ -1,3 +1,17 @@
 namespace ETC {
-  public  class Settings {
-    public List<MixturData> Mixtures
+  public static class Settings {
+    public List<MixturData> Mixtures { get; set; }
+
+    public float MinPpO2 { get; set; }
+
+    public float MaxPpO2Deco { get; set; }
+
+    public float MaxPpO2Dive { get; set; }
+
+    static Settings() {
+      Mixtures = new List<MixtureData>();
+      MinPpO2 = 0.16;
+      MaxPpO2Deco = 1.6;
+      MaxPpO2Dive = 1.4;
+  }
+}
