545394f590a659092ee6996b8e5d281eed205ae7
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 11:36:03 +0200

    Update MixtureData.cs

--- New/MixtureData.cs
+++ New/MixtureData.cs
@@ -2,6 +2,8 @@ namespace ETC.Buehlmann {
   public class MixtureData {
     public List<GasData> Gas { get; } = new List<GasData>();
 
+    public int Usage 
+    
     public MixtureData(params GasData[] pGases) {
       if (pGases?.Lenght > 0) {
         Gas.AddList(pGases);
