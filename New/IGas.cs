0fc7b001e63c84aa249622f2374a75fddb95f79a
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 11:28:40 +0200

    Update IGas.cs

--- New/IGas.cs
+++ New/IGas.cs
@@ -1,10 +1,12 @@
 namespace ETC.Buehlmann {
   public interface IGas {
     string Name { get; init; }
-    
     double StandardGasFraction { get; init; }
-
     GasTypeEnum GasType { get; init; }
-    
+
+    static IEnumerable<T> Enumerate<T() where T : GasEnumBase;
+    bool EqualsAny<T>(params T[] pOthers) where T : GasEnumBase;
+    MixtureGasDTO ToMixtureGas();
+    MixtureGasDTO ToMixtureGas(double pGasFraction);
   }
 }
