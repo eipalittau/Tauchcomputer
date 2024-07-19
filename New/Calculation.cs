97cc35d5aa8325fdd173353b14827406254a9135
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 13:04:37 +0200

    Update Calculation.cs

--- New/Calculation.cs
+++ New/Calculation.cs
@@ -1,20 +1,27 @@
 namespace ETC.Buehlmann {
-  public partial class Calculation {
-    private float _AmbientPressure;
-
-    public List<MixturData> Mixtures 
-
-    public Calculation(float pAmbientPressure) {
-      SetAmbientPressure(pAmbientPressure);
-    }
-    
-    public void SetAmbientPressure(float pAmbientPressure) {
-      _AmbientPressure = pAmbientPressure;
-    }
+  public sealed class Calculation {
+    public List<MixturData> Mixtures;
     
     <summary>Partialdruck im Atemgas bei der Einatmung</summary>
-    private protected PI(float pAmbientPressure) {
+    private PI(float pAmbientPressure) {
       return (pAmbientPressure - Constant.PH2O) * BaseFraction;
-    } 
+    }
+
+    private IEnumerable<MixtureData> GetMixturesWithinPPO2(double pAmbientPressure, MixtureTypeEnum pMixtureType) {
+            List<MixtureData> result = [];
+            double maxPPO2 = pMixtureType == MixtureModeEnum.Dekogas ? Settings.MaximumPPO2Deko : Settings.MaximumPPO2Tg;
+
+            foreach (MixtureData mixture in Mixtures) {
+                if (mixture.Mode == pMode) {
+                    double ppO2 = pAmbientPressure * mixture.MetabolicGas.Fraction;
+
+                    if (ppO2 >= Settings.MinimumPPO2 && ppO2 <= maxPPO2) {
+                        result.Add(mixture);
+                    }
+                }
+            }
+
+            return result;
+        }
   }
 }
