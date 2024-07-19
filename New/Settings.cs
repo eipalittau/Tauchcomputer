8e3685063e56a4231c67a839c6cc084803931928
Patrik Eichmann <eipa@gmx.ch>
2024-07-19 18:16:30 +0200

    Update Settings.cs

--- New/Settings.cs
+++ New/Settings.cs
@@ -1,24 +1,33 @@
 namespace ETC {
   public static class Settings {
-    public List<MixturData> Mixtures { get; set; }
+    public static List<MixturData> Mixtures { get; set; }
 
-    public double MinPpO2 { get; set; }
+    public static double MinPpO2 { get; set; }
 
-    public double MaxPpO2Deco { get; set; }
+    public static double MaxPpO2Deco { get; set; }
 
-    public double MaxPpO2Dive { get; set; }
+    public static double MaxPpO2Dive { get; set; }
 
-    public double MaxPpN2 { get; set; }
+    public static double MaxPpN2 { get; set; }
     
     ///<summary>Respiratorischer Quotient</summary>
-    public double RQ { get; set; }
+    public static double RQ { get; set; }
+
+    ///<summary>Wasserdampfdruck. 47mmHg * 0.00133322 bar/mmHg</summary>
+    public static double WaterVaporPressure { get; set; }
 
     static Settings() {
+      LoadDefaults();
+    }
+
+    public static LoadDefaults() {
       Mixtures = new List<MixtureData>();
       MinPpO2 = 0.16;
       MaxPpO2Deco = 1.6;
       MaxPpO2Dive = 1.4;
       MaxPpN2 = 3.2;
       RQ = 0.8;
+      WaterVaporPressure = 0.06266;
+    }
   }
 }
