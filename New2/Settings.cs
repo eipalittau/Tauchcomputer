3bc74a2ab15ee1a0510f75aa47d236cb8460adbd
Patrik Eichmann <eipa@gmx.ch>
2024-07-21 17:38:56 +0200

    Create Settings.cs

---
+++ New2/Settings.cs
@@ -0,0 +1,12 @@
+namespace ETC {
+  public static class Settings {
+    public Mixture[] Mixtures { get; private set; }
+
+    static Settings() {
+      Mixtures = new Mixture[6];
+      Mixture[0] = new Mixture() {
+        O2 = 0.209
+      }
+    }
+  }
+}
