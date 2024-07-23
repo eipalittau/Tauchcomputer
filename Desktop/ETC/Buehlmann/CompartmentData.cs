3a8f38d39e499ae0cddb0ac96a7095410c7ebaee
Patrik Eichmann <eipa@gmx.ch>
2024-07-23 08:37:36 +0200

    Create CompartmentData.cs

---
+++ Desktop/ETC/Buehlmann/CompartmentData.cs
@@ -0,0 +1,12 @@
+namespace ETC.Buehlmann {
+    public sealed class CompartmentData {
+        ///<summary>Halbwertzeit des Kompartiments</summary>
+        public double HalfLife { get; set; }
+
+        ///<summary>Koeffizient a</summary>
+        public double A { get; set; }
+
+        ///<summary>Koeffizient b</summary>
+        public double B { get; set; }
+    }
+}
