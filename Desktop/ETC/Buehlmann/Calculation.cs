c91fe40c602fa4f6caa63de58b2eb81e75dbf9f7
Patrik Eichmann <eipa@gmx.ch>
2024-07-23 13:50:01 +0200

    Update Calculation.cs

--- Desktop/ETC/Buehlmann/Calculation.cs
+++ Desktop/ETC/Buehlmann/Calculation.cs
@@ -2,6 +2,24 @@ using ETC.Gas;
 
 namespace ETC.Buehlmann {
     public sealed class Calculation {
-        private static CompartmentData[] N2Compartments;
+        private static CompartmentData[] N2Compartments = [ new CompartmentData(  4.0, 1.2599, 0.5050),
+                                                            new CompartmentData(  5.0, 1.1696, 0.5577),
+                                                            new CompartmentData(  8.0, 1.0000, 0.6514),
+                                                            new CompartmentData( 12.5, 0.8618, 0.7222),
+                                                            new CompartmentData( 18.5, 0.7562, 0.7825),
+                                                            new CompartmentData( 27.0, 0.6667, 0.8125),
+                                                            new CompartmentData( 38.3, 0.5933, 0.8434),
+                                                            new CompartmentData( 54.3, 0.5282, 0.8693),
+                                                            new CompartmentData( 77.0, 0.4701, 0.8910),
+                                                            new CompartmentData(109.0, 0.4187, 0.9092),
+                                                            new CompartmentData(146.0, 0.3798, 0.9222),
+                                                            new CompartmentData(187.0, 0.3497, 0.9319),
+                                                            new CompartmentData(239.0, 0.3223, 0.9403),
+                                                            new CompartmentData(305.0, 0.2971, 0.9477),
+                                                            new CompartmentData(390.0, 0.2737, 0.9544),
+                                                            new CompartmentData(498.0, 0.2523, 0.9602),
+                                                            new CompartmentData(635.0, 0.2327, 0.9653) ];
+
+        private static CompartmentData[] HeCompartments =      
     }
 }
