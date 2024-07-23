713625cc66e9aa16a71b4ff4f479e0779b7a6c29
Patrik Eichmann <eipa@gmx.ch>
2024-07-23 14:21:41 +0200

    Create ExpositionData.cs

---
+++ Desktop/ETC/Gas/ExpositionData.cs
@@ -0,0 +1,9 @@
+namespace ETC.Gas {
+  public class ExpositionData {
+    public double PressurePartial { get; set; }
+
+    public double Load { get; set; }
+    
+    public int MaximalExpositionTime { get; set; }
+  }
+}
