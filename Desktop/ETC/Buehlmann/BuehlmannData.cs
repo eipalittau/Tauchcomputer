75e55ac79f04737ab0054cb70262fcac45e94ce9
Patrik Eichmann <eipa@gmx.ch>
2024-07-30 10:26:27 +0200

    Update and rename DiveData.cs to BuehlmannData.cs

---
+++ Desktop/ETC/Buehlmann/BuehlmannData.cs
@@ -0,0 +1,7 @@
+namespace ETC.Buehlmann {
+  public sealed class BuehlmannData {
+    public int NDL { get; set; }
+
+    public int TTS {get; set; }
+  }
+}
--- Desktop/ETC/Buehlmann/DiveData.cs
+++
@@ -1,5 +0,0 @@
-namespace ETC.Buehlmann {
-  public sealed class DiveData {
-    public int NDL { get; set; }
-  }
-}
