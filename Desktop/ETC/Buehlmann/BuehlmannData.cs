c75b339c7f29ab32dbaf4f5e51738c79cb2df541
Patrik Eichmann <eipa@gmx.ch>
2024-07-30 23:26:38 +0200

    Update BuehlmannData.cs

--- Desktop/ETC/Buehlmann/BuehlmannData.cs
+++ Desktop/ETC/Buehlmann/BuehlmannData.cs
@@ -2,6 +2,8 @@ namespace ETC.Buehlmann {
   public sealed class BuehlmannData {
     public int NDL { get; set; }
 
-    public int TTS {get; set; }
+    public int TTS { get; set; }
+
+    public double[,] Deco { get; set; }
   }
 }
