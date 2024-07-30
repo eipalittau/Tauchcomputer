f42df8d536a1efb4e795ba091844a0f3ee5db99e
Patrik Eichmann <eipa@gmx.ch>
2024-07-30 09:56:22 +0200

    Update ContinuousData.cs

--- Desktop/ETC/ContinuousData.cs
+++ Desktop/ETC/ContinuousData.cs
@@ -4,6 +4,8 @@
         public static double CurrentCnsExposition { get; set; } = 0;
 
         public static double[,] CurrentSaturation { get; } = new double[1, 16];
+
+        public static double SurfacePressure { get; set; }
         #endregion
 
         #region Konstruktor
